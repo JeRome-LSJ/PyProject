158178ff42cfc7feb1fe3826e44a3120d58185a7
liusj_0525@163.com <liusj_0525@163.com>
2018-12-02 20:12:22 +0800

    inti

---
+++ src/VariableExample.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author: JeRome
+@Date  : 2018/12/1 6:51 PM
+@Desc  :
+"""
+counter = 100  # 赋值整型变量
+miles = 1000.0  # 浮点型
+name = "John"  # 字符串
+
+print(counter)
+print(miles)
+print(name)
+
+a = b = c = 1
+d, e, f = 1, 2, 'John'
+
+print(a, b, c)
+print(d, e, f)
+
